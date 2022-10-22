f0f41ea3ab6da80f71325057534fa1aefb91fcb5
dobleanix <71346243+dobleanix@users.noreply.github.com>
2022-10-21 19:46:37 -0500

    Add files via upload

--- bot.php
+++ bot.php
@@ -68,7 +68,7 @@ else {
 }
     function send_message($chat_id,$message_id, $message){
         $text = urlencode($message);
-        $apiToken = $_ENV['API_TOKEN'];  
+        $apiToken = $_ENV['5496366652:AAEZ35ZeVdAqPJDte5h4aZih4Gyk-HnM28w'];  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text&parse_mode=HTML");
     }
 ?>
